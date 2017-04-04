bf2c1d1110a5787f5a682b01188bada062acffb1
Nicolas Rod <ncls.rod@gmail.com>
2017-04-04 14:39:25 +0200

    working spammer+

--- src/labo/smtp/Labo_SMTP.java
+++ src/labo/smtp/Labo_SMTP.java
@@ -2,37 +2,59 @@ package labo.smtp;
 
 import java.io.File;
 import java.util.LinkedList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Labo_SMTP {
 
     private final static int SMTP_PORT = 25000;
     private final static String MAIL_SERVER = "localhost";
-    private final static String SENDER_EMAIL = "feur@heig-vd.ch";
-    private final static String RECEIVER_EMAIL = "nicolas.rod@heig-vd.ch";
-    private final static String EMAIL_MESSAGE = "From: bob@areyousure.com\n"
-            + "To: nicolas.rod@heig-vd.ch\n"
-            + "Subject: Demo\n\n"
-            + "THIS IS A TEST FEUR !";
 
     public static void main(String[] args) throws Exception {
 
         // Lecture du fichier
-        System.out.println("Lecture");
-        File f = new File(new File(".").getCanonicalPath() + File.separator
+        System.out.println("Reading email adresses...");
+        File emailAdressesFile = new File(new File(".").getCanonicalPath() + File.separator
                 + "src" + File.separator
                 + "labo" + File.separator
                 + "email_adresses" + File.separator
                 + "email_adresses.txt" + File.separator);
+        
+        File[] emailMessageFiles = new File(new File(".").getCanonicalPath() + File.separator
+                + "src" + File.separator
+                + "labo" + File.separator
+                + "email_messages" + File.separator).listFiles();
+        
+        
+        
+        String[] messages = new String[emailMessageFiles.length];
+        
+        for (int i = 0; i < emailMessageFiles.length; i++){
+            String tmp = "";
+            Scanner scanner = new Scanner(emailMessageFiles[i]);
+
+            while (scanner.hasNextLine()) {
+                tmp += scanner.nextLine() + "\n";
+            }
+            messages[i] = tmp;
+        }
+        
+        System.out.println(messages[0]);
+        System.out.println(messages[1]);
 
         // Construction des groupes
-        System.out.println("Construction");
-        LinkedList<SMTPSpamGroup> groups = new SMTPSpamGroupGenerator(f).generate(2);
+        System.out.println("Constructing groups...");
+        LinkedList<SMTPSpamGroup> groups = new SMTPSpamGroupGenerator(emailAdressesFile).generate(2);
 
-        // itération sur chaque groupe
-        System.out.println("Itération");
+        // Itération sur chaque groupe
+        System.out.println("Sending spam");
+        Random randomGenerator = new Random();
+        
         for (Object o : groups) {
             SMTPSpamGroup group = (SMTPSpamGroup)o;
-            SMTPSpamHandler s = new SMTPSpamHandler(SMTP_PORT, MAIL_SERVER, group, EMAIL_MESSAGE);
+            int randomInt = randomGenerator.nextInt(messages.length);
+            System.out.println(randomInt);
+            SMTPSpamHandler s = new SMTPSpamHandler(SMTP_PORT, MAIL_SERVER, group, messages[randomInt]);
             s.spam();
         }
     }
